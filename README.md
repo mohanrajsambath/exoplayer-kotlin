b9af25151fb466e031c0749d631dab4c86010f9d
yoobi <>
2020-01-31 16:57:59 +0100

    Develop: Quality Selector

--- README.md
+++ README.md
@@ -4,6 +4,8 @@ Example of Exoplayer v2.11.1 in Kotlin
 
 This example is for HLS video
 
+Added quality selector thanks to [Exoplayer demo](https://github.com/google/ExoPlayer/tree/release-v2/demos/main/src/main/java/com/google/android/exoplayer2/demo)
+
 # Resources
 
 [Codelabs](https://codelabs.developers.google.com/codelabs/exoplayer-intro/#0)
--- app/src/main/java/com/app/exoplayer_kotlin/MainActivity.kt
+++ app/src/main/java/com/app/exoplayer_kotlin/MainActivity.kt
@@ -1,30 +1,43 @@
 package com.app.exoplayer_kotlin
 
+import android.app.Dialog
 import android.net.Uri
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.exoplayer2.C
+import com.google.android.exoplayer2.Player
 import com.google.android.exoplayer2.SimpleExoPlayer
 import com.google.android.exoplayer2.source.hls.HlsMediaSource
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector
+import com.google.android.exoplayer2.trackselection.MappingTrackSelector
+import com.google.android.exoplayer2.ui.TrackSelectionDialogBuilder
 import com.google.android.exoplayer2.upstream.DataSource
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
 import com.google.android.exoplayer2.util.Util
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.exo_playback_control_view.*
 
-    const val HLS_STATIC_URL = "https://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8"
-    const val STATE_RESUME_WINDOW = "resumeWindow"
-    const val STATE_RESUME_POSITION = "resumePosition"
-    const val STATE_PLAYER_FULLSCREEN = "playerFullscreen"
-    const val STATE_PLAYER_PLAYING = "playerOnPlay"
+const val HLS_STATIC_URL = "https://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8"
+const val STATE_RESUME_WINDOW = "resumeWindow"
+const val STATE_RESUME_POSITION = "resumePosition"
+const val STATE_PLAYER_FULLSCREEN = "playerFullscreen"
+const val STATE_PLAYER_PLAYING = "playerOnPlay"
+const val MAX_HEIGHT = 539
+const val MAX_WIDTH = 959
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var exoPlayer: SimpleExoPlayer
     private lateinit var dataSourceFactory: DataSource.Factory
+    private lateinit var trackSelector: DefaultTrackSelector
 
     private var currentWindow = 0
     private var playbackPosition: Long = 0
     private var isFullscreen = false
     private var isPlayerPlaying = true
+    private var trackDialog: Dialog? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,6 +45,13 @@ class MainActivity : AppCompatActivity() {
 
         dataSourceFactory = DefaultDataSourceFactory(this,
             Util.getUserAgent(this, "testapp"))
+
+        exo_quality.setOnClickListener{
+            if(trackDialog == null){
+                initPopupQuality()
+            }
+            trackDialog?.show()
+        }
 
         if (savedInstanceState != null) {
             currentWindow = savedInstanceState.getInt(STATE_RESUME_WINDOW)
@@ -42,7 +62,11 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun initPlayer(){
-        exoPlayer = SimpleExoPlayer.Builder(this).build()
+
+        trackSelector = DefaultTrackSelector(this)
+        // When player is initialized it'll be played with a quality of MaxVideoSize to prevent loading in 1080p from the start
+        trackSelector.setParameters(trackSelector.buildUponParameters().setMaxVideoSize(MAX_WIDTH,MAX_HEIGHT))
+        exoPlayer = SimpleExoPlayer.Builder(this).setTrackSelector(trackSelector).build()
         val videoSource = HlsMediaSource.Factory(dataSourceFactory)
             .createMediaSource(Uri.parse(HLS_STATIC_URL))
 
@@ -53,6 +77,14 @@ class MainActivity : AppCompatActivity() {
         }
         player_view.player = exoPlayer
 
+        //Listener on player
+        exoPlayer.addListener(object: Player.EventListener{
+            override fun onPlayerStateChanged(playWhenReady: Boolean, playbackState: Int) {
+                if(playbackState == Player.STATE_READY){
+                    exo_quality.visibility = View.VISIBLE
+                }
+            }
+        })
     }
 
     private fun releasePlayer(){
@@ -101,4 +133,41 @@ class MainActivity : AppCompatActivity() {
             releasePlayer()
         }
     }
+
+    // QUALITY SELECTOR
+
+    private fun initPopupQuality() {
+        val mappedTrackInfo = trackSelector.currentMappedTrackInfo
+        var videoRenderer : Int? = null
+
+        if(mappedTrackInfo == null) return else exo_quality.visibility = View.VISIBLE
+
+        for(i in 0 until mappedTrackInfo.rendererCount){
+            if(isVideoRenderer(mappedTrackInfo, i)){
+                videoRenderer = i
+            }
+        }
+
+        if(videoRenderer == null){
+            exo_quality.visibility = View.GONE
+            return
+        }
+
+        val trackSelectionDialogBuilder = TrackSelectionDialogBuilder(this, getString(R.string.qualitySelector), trackSelector, videoRenderer)
+        trackSelectionDialogBuilder.setTrackNameProvider{
+            // Override function getTrackName
+            getString(R.string.exo_track_resolution_pixel, it.height)
+        }
+        trackDialog = trackSelectionDialogBuilder.build()
+    }
+
+    private fun isVideoRenderer(mappedTrackInfo: MappingTrackSelector.MappedTrackInfo, rendererIndex: Int): Boolean {
+        val trackGroupArray = mappedTrackInfo.getTrackGroups(rendererIndex)
+        if (trackGroupArray.length == 0) {
+            return false
+        }
+        val trackType = mappedTrackInfo.getRendererType(rendererIndex)
+        return C.TRACK_TYPE_VIDEO == trackType
+    }
+
 }
