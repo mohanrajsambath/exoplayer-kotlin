258da10f473fbb36b3f6028ac4c9a418f7848cb0
Yoni Obadia <yobadia@hexaglobe.com>
2020-05-29 22:22:09 +0200

    dev: thumbpreview

--- app/src/main/java/com/app/exoplayer_kotlin/MainActivity.kt
+++ app/src/main/java/com/app/exoplayer_kotlin/MainActivity.kt
@@ -2,15 +2,26 @@ package com.app.exoplayer_kotlin
 
 import android.net.Uri
 import android.os.Bundle
+import android.util.DisplayMetrics
+import android.view.View
+import android.view.ViewGroup
+import android.view.ViewGroup.MarginLayoutParams
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.request.RequestOptions
+import com.bumptech.glide.request.target.Target
 import com.google.android.exoplayer2.SimpleExoPlayer
 import com.google.android.exoplayer2.source.hls.HlsMediaSource
+import com.google.android.exoplayer2.ui.TimeBar
 import com.google.android.exoplayer2.upstream.DataSource
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
 import com.google.android.exoplayer2.util.Util
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.exo_playback_control_view.*
 
     const val HLS_STATIC_URL = "https://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8"
+    const val THUMBNAIL_MOSAIQUE = "https://bitdash-a.akamaihd.net/content/MI201109210084_1/thumbnails/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.jpg"
     const val STATE_RESUME_WINDOW = "resumeWindow"
     const val STATE_RESUME_POSITION = "resumePosition"
     const val STATE_PLAYER_FULLSCREEN = "playerFullscreen"
@@ -53,6 +64,61 @@ class MainActivity : AppCompatActivity() {
         }
         player_view.player = exoPlayer
 
+        exo_progress.addListener(object : TimeBar.OnScrubListener {
+            override fun onScrubMove(timeBar: TimeBar, position: Long) {
+                previewFrameLayout.visibility = View.VISIBLE
+                previewFrameLayout.x = updatePreviewX(position.toInt(), exoPlayer.duration.toInt()).toFloat()
+
+                val drawable = imageView.drawable
+                var glideOptions = RequestOptions().dontAnimate().skipMemoryCache(false)
+                if (drawable != null) {
+                    glideOptions = glideOptions.placeholder(drawable)
+                }
+
+                Glide.with(imageView).asBitmap()
+                    .apply(glideOptions)
+                    .load(THUMBNAIL_MOSAIQUE)
+                    .override(Target.SIZE_ORIGINAL ,Target.SIZE_ORIGINAL)
+                    .transform(GlideThumbnailTransformation(position))
+                    .diskCacheStrategy(DiskCacheStrategy.RESOURCE)
+                    .into(imageView)
+            }
+
+            override fun onScrubStop(timeBar: TimeBar, position: Long, canceled: Boolean) {
+                previewFrameLayout.visibility = View.INVISIBLE
+            }
+
+            override fun onScrubStart(timeBar: TimeBar, position: Long) {}
+        })
+
+    }
+
+    private fun updatePreviewX(progress: Int, max: Int): Int {
+        if (max == 0) { return 0 }
+
+        val parent = previewFrameLayout.parent as ViewGroup
+        val layoutParams = previewFrameLayout.layoutParams as MarginLayoutParams
+        val offset = progress.toFloat() / max
+        val minimumX: Int = previewFrameLayout.left
+        val maximumX = (parent.width - parent.paddingRight - layoutParams.rightMargin)
+
+        val previewPaddingRadius: Int = resources.getDimensionPixelSize(R.dimen.scrubber_dragged_size).div(2)
+        val previewLeftX = (exo_progress as View).left.toFloat()
+        val previewRightX = (exo_progress as View).right.toFloat()
+        val previewSeekBarStartX: Float = previewLeftX + previewPaddingRadius
+        val previewSeekBarEndX: Float = previewRightX - previewPaddingRadius
+        val currentX = (previewSeekBarStartX + (previewSeekBarEndX - previewSeekBarStartX) * offset)
+        val startX: Float = currentX - previewFrameLayout.width / 2f
+        val endX: Float = startX + previewFrameLayout.width
+
+        // Clamp the moves
+        return if (startX >= minimumX && endX <= maximumX) {
+            startX.toInt()
+        } else if (startX < minimumX) {
+            minimumX
+        } else {
+            maximumX - previewFrameLayout.width
+        }
     }
 
     private fun releasePlayer(){
